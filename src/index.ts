8f4eff570ef4e9cb8e70427c283cb8c0bab6aff1
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-05-29 17:02:03 +0545

    Wrap index inside try catch
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,28 @@ import config from './config';
 import routes from './routes';
 import { initializeIndices } from './utils/elastic-search.util';
 import { genericErrorHandler, routeNotFound } from './middlewares/error-handler.middleware';
+import logger from './utils/logger.util';
 
 (async function () {
-  const isElasticsearchReady = await initializeIndices();
+  try {
+    const isElasticsearchReady = await initializeIndices();
 
-  if (isElasticsearchReady) {
-    const app = express();
-    const port = config.app.port;
-
-    app.use(cors());
-    app.use(compression());
-    app.use(bodyParser.json());
-
-    app.use('/api', routes);
-
-    app.use(routeNotFound);
-    app.use(genericErrorHandler);
-
-    app.listen(port, () => console.log(`Application running on ${config.app.url}`));
+    if (isElasticsearchReady) {
+      const app = express();
+      const port = config.app.port;
+  
+      app.use(cors());
+      app.use(compression());
+      app.use(bodyParser.json());
+  
+      app.use('/api', routes);
+  
+      app.use(routeNotFound);
+      app.use(genericErrorHandler);
+  
+      app.listen(port, () => console.log(`Application running on ${config.app.url}`));
+    }
+  } catch (err) {
+    logger.error(`Something went wrong >> ${err}`);
   }
 })();
